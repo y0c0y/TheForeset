fde5851ae775c83b28c478e4e3561927077c2e15
Cho Yunhee <68114427+y0c0y@users.noreply.github.com>
2025-03-19 22:44:19 +0900

    feat : Object 제작

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,34 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { private set; get; }
+    
+    public event Action<bool> OnGameOverCanvas;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        OnGameOverCanvas?.Invoke(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void GameOver()
+    {
+        OnGameOverCanvas?.Invoke(true);
     }
 }
