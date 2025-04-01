2877ba8125dfaf135f6a7420e982f2cbe25e8c3f
ᓚᘏᗢ <choiashley0207@gmail.com>
2025-04-01 10:08:50 +0900

    feat : 시작화면 수정 (거미추가)

--- Assets/Scripts/StartFadeOut.cs
+++ Assets/Scripts/StartFadeOut.cs
@@ -12,6 +12,10 @@ public class StartImage : MonoBehaviour
     private CanvasGroup CanvasGroup;
     //
     private GameObject GM;
+    //
+    private GameObject SP;
+    //
+    public Material mat;
     //이미지가 사라지는데 걸리는 시간 (초)
     public float fadeDuration = 1f;
     //시간이 얼마나 흘렀는지 저장하는 변수
@@ -28,7 +32,11 @@ public class StartImage : MonoBehaviour
         Time.timeScale = 0f;
         CanvasGroup = canvas.GetComponent<CanvasGroup>();
         GM = GameObject.Find("GM");
+        SP = GameObject.Find("StartSP");
         originalAlpha = CanvasGroup.alpha;
+        mat.color = new Color(1f, 1f, 1f, 1f);
+        // Material mat;
+        // mat.color = new Color(1f, 1f, 1f, 0.8f);
     }
 
     void Update()
@@ -47,21 +55,24 @@ public class StartImage : MonoBehaviour
             timer += Time.unscaledDeltaTime;
             
             float alpha = Mathf.Lerp(originalAlpha, 0f, timer / fadeDuration);
-            // Debug.Log($"{timer} -> {alpha}");
+            float matAlpha = Mathf.Lerp(1f, 0.85f, timer / fadeDuration);
             
             // Color color = fadeImage.color;
             // color.a = alpha;
             // fadeImage.color = color;
             
+            mat.color = new Color(1, 1, 1, matAlpha);
             CanvasGroup.alpha = alpha;
 
             if (alpha <= 0f)
             {
                 isfading = false;
-                // canvas.gameObject.SetActive(false);
                 GM.SetActive(false);
+                SP.SetActive(false);
+                mat.color = new Color(1f, 1f, 1f, 1f);
                 Time.timeScale = 1f;
             }
+            
         }
     }
     
