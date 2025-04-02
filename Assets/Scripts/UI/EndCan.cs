452bd17901a88225fd299b7034afb0c9f5ae0831
ᓚᘏᗢ <choiashley0207@gmail.com>
2025-04-02 15:36:58 +0900

    feat 대규모 추가

---
+++ Assets/Scripts/UI/EndCan.cs
@@ -0,0 +1,69 @@
+using System;
+using UnityEngine;
+
+public class EndCan : MonoBehaviour
+{
+    public Talk talk;
+    public CanvasGroup canvasGroup;
+    public CanvasGroup endText;
+    private bool isFading;
+    private bool startEndText = false;
+    public float fadeSpeed = 0.5f;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isFading = true;
+        }
+    }
+
+    private void Start()
+    {
+        canvasGroup.alpha = 0f;
+        endText.alpha = 0f;
+    }
+
+    void Update()
+    {
+        if (isFading)
+        {
+            Time.timeScale = 0f;
+            canvasGroup.alpha += fadeSpeed * Time.unscaledDeltaTime;
+
+            if (canvasGroup.alpha >= 1f)
+            {
+                canvasGroup.alpha = 1f;
+
+                talk.Play(
+                    2f,
+                    new[]
+                    {
+                        "후.. 드디어 집이다..",
+                        "정말 이상하고 무서운 하루였어..",
+                        "으! 생각하기 싫다 얼른 자야지!!"
+                    });
+                isFading = false;
+                startEndText = true; 
+            }
+        }
+
+        if (startEndText)
+        {
+            if (talk.IsAllEnd)
+            {
+                endText.alpha += fadeSpeed * Time.unscaledDeltaTime;
+
+                if (endText.alpha >= 1f)
+                {
+                    endText.alpha = 1f;
+                    startEndText = false;
+                }
+
+            }
+
+        }
+
+    }
+}
